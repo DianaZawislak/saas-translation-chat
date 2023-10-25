ab4eb8e15151477988dcbe52d20dc79d0b3f85f6
dianazawislak <diana.zawislak@icloud.com>
2023-10-25 18:05:18 -0400

    installed shadecn-ui

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,11 +1,20 @@
 import Link from 'next/link'
 import React from 'react'
 import LogoImage from '@logos/logo1.png'
+import { AspectRatio } from './ui/aspect-ratio'
 
 function Logo() {
   return (
     <Link href="/">
-      Go Home
+    <div>
+      <AspectRatio
+      ratio={16/9}
+      className="flex items-center justify-center"
+    >
+      <LogoImage />
+
+    </AspectRatio>
+    </div>
     </Link>
   )
 }
