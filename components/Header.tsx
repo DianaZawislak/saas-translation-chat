e26dac0771be29098d5bc0056c29bba96947cc3b
Diana Zawislak <diana.zawislak@icloud.com>
2023-11-01 21:38:42 -0400

    header update

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,37 +8,38 @@ import DarkModeToggle from "./DarkModeToggle";
 import UpgradeBanner from "./UpgradeBanner";
 import LanguageSelect from "./LanguageSelect";
 import CreateChatButton from "./CreateChatButton";
-function Header() {
- 
-    return (
-        <header className="sticky top-0 z-50">
-          <nav className="flex flex-col sm:flex-row items-center p-5 pl-2  dark:bg-black  max-w-7xl mx-auto">
-            <Logo />
-    
-            <div className="flex-1 flex items-center justify-end  space-x-4">
-               <LanguageSelect />
-    
-              {session ? (
-                <>
-                  <Link href="/chat" prefetch={false}>
-                    <MessagesSquareIcon className="text-black dark:text-white" />
-                  </Link>
-                  <CreateChatButton />
-                </>
-              ) : (
-                <Link href={"/pricing"} prefetch={false}>
-                  Pricing
-                </Link>
-              )} 
-                <DarkModeToggle />
-               <UserButton session={session} />
-            </div>
-          </nav>
-    
-          <UpgradeBanner /> 
-        </header>
-      );
-    }
-    
-    export default Header;
 
+async function Header() {
+  const session = await getServerSession(authOptions);
+
+  return (
+    <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
+      <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900  max-w-7xl mx-auto">
+        <Logo />
+
+        <div className="flex-1 flex items-center justify-end  space-x-4">
+          <LanguageSelect />
+
+          {session ? (
+            <>
+              <Link href="/chat" prefetch={false}>
+                <MessagesSquareIcon className="text-black dark:text-white" />
+              </Link>
+              <CreateChatButton />
+            </>
+          ) : (
+            <Link href={"/pricing"} prefetch={false}>
+              Pricing
+            </Link>
+          )}
+          <DarkModeToggle />
+          <UserButton session={session} />
+        </div>
+      </nav>
+
+      <UpgradeBanner />
+    </header>
+  );
+}
+
+export default Header;
